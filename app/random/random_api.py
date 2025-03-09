f261d270d40febc24d94fb01a6a7620a8b4a0793
Hom <tobe132546@gmail.com>
2025-03-10 06:29:47 +0700

    feat:secure code

--- app/random/random_api.py
+++ app/random/random_api.py
@@ -2,12 +2,14 @@ import os
 import random
 from fastapi import APIRouter, Query, HTTPException
 from fastapi.responses import FileResponse
-
+from pathlib import Path
 
 
 random_router = APIRouter()
 
-base_path = "Images"
+BASE_IMAGE_PATH = Path("images")
+
+ALLOWED_EXTENSIONS = {".jpg", ".jpeg", ".png", ".gif", ".bmp"}
 
 @random_router.get('/get-images-list')
 def get_random_images_from_dirs():
@@ -15,8 +17,8 @@ def get_random_images_from_dirs():
     random_image_paths = []
 
     try:
-        for dir_name in os.listdir(base_path):
-            dir_path = os.path.join(base_path, dir_name)
+        for dir_name in os.listdir(BASE_IMAGE_PATH):
+            dir_path = os.path.join(BASE_IMAGE_PATH, dir_name)
 
             if os.path.isdir(dir_path):  
                 image_files = [f for f in os.listdir(dir_path) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
@@ -34,12 +36,18 @@ def get_random_images_from_dirs():
 
 @random_router.get('/get-image')
 def get_image(image_path : str = Query(..., description="Path to the image")):
-    print(image_path)
-    try:
-        if os.path.exists(image_path) and os.path.isfile(image_path):
-            return FileResponse(image_path)
-        else:
-            raise HTTPException(status_code=404, detail="Image not found")
 
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error: {e}")
+    sanitized_path = BASE_IMAGE_PATH / Path(image_path).name
+
+    print(Path(image_path).name)
+
+    if not sanitized_path.is_relative_to(BASE_IMAGE_PATH):
+            raise HTTPException(status_code=403, detail="Forbidden: Access to this file is not allowed.")
+
+    if sanitized_path.exists() and sanitized_path.is_file():
+        file_extension = sanitized_path.suffix.lower()
+        if file_extension not in ALLOWED_EXTENSIONS:
+            raise HTTPException(status_code=415, detail="Unsupported file type.")
+        return FileResponse(sanitized_path)
+    else:
+        raise HTTPException(status_code=404, detail="Image not found")
