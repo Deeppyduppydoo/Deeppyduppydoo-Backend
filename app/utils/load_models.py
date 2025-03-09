25ee677eee4ca89b6fc1e5288d519ef4d93b7ef3
Katapkoon <katapat80@gmail.com>
2025-03-09 20:23:23 +0700

    feat: add Mbv3 model

--- app/utils/load_models.py
+++ app/utils/load_models.py
@@ -1,5 +1,7 @@
-from app.models.model import TensorflowModel
+from app.models.model import TensorflowModel, Pytorch_MBv3
 
 Vgg16 = TensorflowModel('save_models/model_Augment_2.h5')
 
 Vgg16.loadModelTensorflow()
+
+Mbv3 = Pytorch_MBv3('save_models/mbv3-large-pretrained-imagenet1k-finetuned-handwriting.pth')
