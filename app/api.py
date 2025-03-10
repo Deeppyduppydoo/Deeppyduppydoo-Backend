4447ded6ddd94a0698b5c25a0c5b0272ea41c33c
johnybravou2 <128812874+johnybravou2@users.noreply.github.com>
2025-03-10 17:47:17 +0700

    cors policy

--- app/api.py
+++ app/api.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.predict.predict_api import router
 from app.random.random_api import random_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 def create_app() -> FastAPI:
     app = FastAPI(
@@ -16,6 +18,14 @@ app = create_app()
 app.include_router(router=router)
 
 app.include_router(router=random_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # You can specify a list of allowed origins like ["http://localhost:3000"]
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 @app.get("/")
 async def read_root():
