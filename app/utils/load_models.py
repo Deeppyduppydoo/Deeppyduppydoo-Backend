7db07cae958e85a60ce47f6433418631a1b6446c
Hom <tobe132546@gmail.com>
2025-03-10 14:58:49 +0700

    chore:change dir

--- app/utils/load_models.py
+++ app/utils/load_models.py
@@ -8,4 +8,4 @@ Mbv3 = Pytorch_MBv3()
 Mbv3.loadModel('save_models/mbv3-large-pretrained-imagenet1k-finetuned-handwriting.pth')
 
 Resnet18 = Pytorch_ResNet()
-Resnet18.loadModel('saves/models/handwriting_resnet.pth')
+Resnet18.loadModel('save_models/handwriting_resnet.pth')
